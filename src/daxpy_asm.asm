f0795f178f9df1dbb7653878bbca14e3de3daaac
BautistaRalphh <bautista.ralphgabriel@gmail.com>
2024-11-22 12:11:48 +0800

    Add files via upload

---
+++ src/daxpy_asm.asm
@@ -0,0 +1,15 @@
+;Ralph Gabriel R. Bautista S15
+;Ghee Kaye S. Lopez S15
+
+%include "io64.inc"
+
+section .data
+
+section .bss
+
+section .text
+global main
+main:
+
+    xor rax, rax                    
+    ret
